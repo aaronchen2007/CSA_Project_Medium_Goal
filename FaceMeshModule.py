465d469ccfa976a7470ce8187598ad039ac05204
aaronchen2007 <aaronchen20072007@gmail.com>
2024-06-12 16:54:54 +0800

    Continue learning to create the FaceMeshModule

--- FaceMeshModule.py
+++ FaceMeshModule.py
@@ -8,25 +8,33 @@ class FaceMeshDetector():
     def __init__(self,staticMode = False, maxFaces = 2, minDetectionCon = 0.5, minTrackCon = 0.5):
 
         self.staticMode = staticMode
-        self.maxFace = maxFaces
+        self.maxFaces = maxFaces
         self.minDetectionCon = minDetectionCon
         self.minTrackCon = minTrackCon
 
         self.mpDraw = mp.solutions.drawing_utils
         self.mpFaceMesh = mp.solutions.face_mesh
-        self.faceMesh = self.mpFaceMesh.FaceMesh(max_num_faces=2)
-        self.drawSpec = mpDraw.DrawingSpec(thickness=1, circle_radius=2)
+        self.faceMesh = self.mpFaceMesh.FaceMesh(
+            static_image_mode=self.staticMode,
+            max_num_faces= self.maxFaces,
+            min_detection_confidence=self.minDetectionCon,
+            min_tracking_confidence=self.minTrackCon
+        )
+        self.drawSpec = self.mpDraw.DrawingSpec(thickness=1, circle_radius=2)
 
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = faceMesh.process(imgRGB)
-    if results.multi_face_landmarks:
-        for faceLms in results.multi_face_landmarks:
-            mpDraw.draw_landmarks(img, faceLms, mpFaceMesh.FACEMESH_CONTOURS, drawSpec, drawSpec)
+    def findFaceMesh(self, img, draw = True):
+        self.imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.faceMesh.process(self.imgRGB)
+        if self.results.multi_face_landmarks:
+            for faceLms in self.results.multi_face_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, faceLms, self.mpFaceMesh.FACEMESH_CONTOURS, self.drawSpec, self.drawSpec)
 
-            for id,lm in enumerate(faceLms.landmark):
-                ih, iw, ic = img.shape
-                x, y = int(lm.x * iw), int(lm.y * ih)
-                print(id, x,y)
+                # for id, lm in enumerate(faceLms.landmark):
+                #     ih, iw, ic = img.shape
+                #     x, y = int(lm.x * iw), int(lm.y * ih)
+                #     print(id, x, y)
+        return img
 
 
 
@@ -34,8 +42,10 @@ class FaceMeshDetector():
 def main():
     cap = cv2.VideoCapture(0)
     pTime = 0
+    detector = FaceMeshDetector()
     while True:
         success, img = cap.read()
+        img = detector.findFaceMesh(img, draw=True)
         cTime = time.time()
         fps = 1 / (cTime - pTime)
         pTime = cTime
